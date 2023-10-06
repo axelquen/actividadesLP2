79249005516d4a58678cd8716d93c2ed6a0003a9
axelquen <145068363+axelquen@users.noreply.github.com>
2023-10-06 08:48:07 -0500

    Add files via upload

---
+++ Pokemon_Herencia/Pokemon.java
@@ -0,0 +1,44 @@
+public class Pokemon{
+    private int HP, nivel;
+    private String nombre, tipo;
+
+    public Pokemon(String nombre, String tipo, int nivel){
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.nivel = nivel;
+        HP = 100;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    private void calculaDanio(int danio){
+        this.HP -= danio;
+        System.out.printf("%s recibe %d puntos de danio\n",
+        this.getNombre(), danio);
+    }
+
+    public void recibirAtaque(String movimiento) {
+        System.out.printf("%s recibe ATK %s\n", this.getNombre(), movimiento);
+        calculaDanio((int) Math.random() * 10 + 1);
+    }
+
+    public void atacar(String movimiento, Pokemon pokemon) {
+        System.out.printf("%s ataca a %s con %s\n", this.getNombre(), pokemon.getNombre(), movimiento);
+        pokemon.recibirAtaque(movimiento);
+    }
+    
+}
